f4d3a915068c9dbb74e5753f11ad9b72209f5fc3
mojalefamj <mojalefaobed@yahoo.com>
2021-01-14 23:21:03 +0200

    task0.1

---
+++ Task1.java
@@ -0,0 +1,21 @@
+public class Task1
+{
+	public static void main (String[] args)
+	{
+		int x = 0;
+		int y = 1;
+
+		System.out.print(x);
+		System.out.print("\n");
+		System.out.print(y);
+		System.out.print("\n");
+
+		x += 3;
+		y += x;
+
+		System.out.print(x);
+		System.out.print("\n");
+		System.out.print(y);
+		System.out.print("\n");
+	}
+}
