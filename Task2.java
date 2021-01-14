293414a4100df956cd12c67521d129a56c991229
mojalefamj <mojalefaobed@yahoo.com>
2021-01-14 23:32:41 +0200

    task 0.2

---
+++ Task2.java
@@ -0,0 +1,13 @@
+public class Task2
+{
+	public static void main (String[] args)
+	{
+		int x = 1 + 1 *2;
+		int y = (1+1) * 2;
+		int z = 1 + (1 * 2);
+		int a = 1 + 1 * 2 / 2;
+		int b = (1 + 1 * 2) / 2;
+
+		System.out.print(x + "\n" + y + "\n" + z + "\n" + a + "\n" + b);
+	}
+}
