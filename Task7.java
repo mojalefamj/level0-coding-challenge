09de2bc9d1ce478bb07fae6d8a21bfaee28398d2
mojalefamj <mojalefaobed@yahoo.com>
2021-01-15 18:33:09 +0200

    task 0.7

---
+++ Task7.java
@@ -0,0 +1,12 @@
+public class Task7
+{
+	public static double temp (double celsius)
+	{
+		return celsius * 9 / 5 + 32;
+	}
+
+	public static void main (String[] args)
+	{
+		System.out.print(temp(13));
+	}
+}
