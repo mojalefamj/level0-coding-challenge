36a5df8b4536f753714b1daa9a6c997efbeff047
mojalefamj <mojalefaobed@yahoo.com>
2021-01-15 09:07:36 +0200

    task 0.4

---
+++ Task4.java
@@ -0,0 +1,20 @@
+public class Task4
+{
+	public static void evenOrOdd (int num)
+	{
+
+		if (num % 2 == 0)
+		{
+			System.out.print("Even");
+		}
+		else
+		{
+			System.out.print("Odd");
+		}
+	}
+
+	public static void main (String[] args)
+	{
+		evenOrOdd(8);
+	}
+}
