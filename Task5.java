c9b763dbde714b2e4bcfe9daee3401c3f54e777c
mojalefamj <mojalefaobed@yahoo.com>
2021-01-15 10:02:17 +0200

    task 0.5

---
+++ Task5.java
@@ -0,0 +1,18 @@
+public class Task5
+{
+	public static double areaOfTriangle (float num1, float num2, float num3)
+	{
+		double s = (num1 + num2 + num3) /2;
+
+		double area = Math.sqrt(s * (s - num1) * (s - num2) * (s - num3));
+		
+		return area;
+	}
+
+	public static void main (String[] args)
+	{
+		double results = areaOfTriangle(10,10,10);
+
+		System.out.print("Area of this triangle is : " + results);
+	}
+}
