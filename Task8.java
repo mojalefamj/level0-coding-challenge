e0367b75a04130d6c1ac9363e051f3e2537d09b9
Mojalefa <mojalefaobed@yahoo.com>
2021-08-02 23:27:17 +0200

    hour fix

--- Task8.java
+++ Task8.java
@@ -3,15 +3,12 @@ public class Task8 {
 		int hours = x / 60;
 		int mins = x % 60;
 		
-		if (hours > 1) {
-			if (mins > 1) {
-				System.out.println(hours + " hours, " + mins + " minutes");
+		if (hours == 1) {
+			if (mins == 1) {
+				System.out.println(hours + " hour, " + mins + " minute");
 			}
 			else
-				System.out.println(hours + " hours, " + mins + " minutes");
-		}
-		else if (mins > 1) {
-			System.out.println(hours + " hours, " + mins + " minutes");
+				System.out.println(hours + " hour, " + mins + " minutes");
 		}
 		else {
 			System.out.println(hours + " hours, " + mins + " minutes");
